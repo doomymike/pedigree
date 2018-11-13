bced5de30104ab3901dc49cf2b9728fff4727a3a
Tanngent <maxtang77@gmail.com>
2018-11-13 10:50:37 -0500

    Added initialization for tree
    Added sex.
    Changed some methods to be more efficient.
    I think the structure should be a arraylist of arraylists with connections stored with the people, to make traversing up, down and across the tree the easiest. We should focus on the actual probabilities first, before we worry about the names and stuff.

--- src/Tree.java
+++ src/Tree.java
@@ -1,19 +1,40 @@
 import java.util.ArrayList;
 
 public class Tree {
+	ArrayList<ArrayList<Person>> all = new ArrayList<ArrayList<Person>>();
 	
+	Tree(){
+		Person initialPerson = new Person();
+		Person initialMother = new Person();
+		initialMother.sex = true;
+		Person initialFather = new Person();
+		
+		initialFather.addPartner(initialMother);
+		initialFather.addChild(initialPerson);
+		initialPerson.addParents(initialMother, initialFather);
+		
+		ArrayList<Person> initialParents = new ArrayList<Person>();
+		initialParents.add(initialFather);
+		initialParents.add(initialMother);
+		ArrayList<Person> initialPersons = new ArrayList<Person>();
+		initialPersons.add(initialPerson);
+		all.add(initialPersons);
+		all.add(initialParents);
+	}
 }
 
 class Person{
 	ArrayList<Person> children;
 	Person mother;
 	Person father;
+	Person spouse;
+	boolean sex; // false = male, true = female
 	boolean affected;
 	double homozygousAffected;
 	double heterozygous;
-	double homosygousUnaffected;
-	String name;
-	Person spouse;
+	double homozygousUnaffected;
+	//String name;
+	
 	
 	Person(){
 		
@@ -22,31 +43,34 @@ class Person{
 		this.affected = affected;
 	}
 	
+	/*
 	Person(String name){
 		this.name = name;
 	}
+	*/
 	
-	//maybe remove
+	/*maybe remove
 	Person(String mother,String father){
 		this.father = new Person(father);
 		this.mother = new Person(mother);
 		this.father.spouse = this.mother;
 		this.mother.spouse = this.father;
 	}
+	*/
 	
-	void addChild(String child){
-		this.children.add(new Person(child));
+	void addChild(Person child){
+		this.children.add(child);
+		this.spouse.children.add(child);
 	}
 	
-	void addPartner(String partner) {
-		this.spouse = new Person(partner);
+	void addPartner(Person partner) {
+		this.spouse = partner;
+		partner.spouse = this;
 	}
 	
-	void addParents(String mother,String father) {
-		this.father = new Person(father);
-		this.mother = new Person(mother);
-		this.father.spouse = this.mother;
-		this.mother.spouse = this.father;
+	void addParents(Person mother,Person father) {
+		this.father = father;
+		this.mother = mother;
 	}
 	
 }
