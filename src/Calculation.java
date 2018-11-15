82967f6c4882a72240e8d4b7a34ad28f43e3fd51
doomymike <34379835+doomymike@users.noreply.github.com>
2018-11-15 11:28:44 -0500

    Update Calculation.java

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,11 +1,17 @@
 
+import java.util.Arrays;
+
 public class Calculation {
 	String[] simpleCalc(Person x){
+		char[] sort =new char[2];
 		String[] options = new String[4];
 		int temp =0;
 		for(int i =0;i<2;i++){
 			for(int j = 0;j<2;j++){
-				options[temp] = ""+x.getFather().getCode()[i]+x.getMother().getCode()[j];
+				sort[0] = x.getFather().getCode()[i];
+				sort[1] = x.getMother().getCode()[j];
+				Arrays.sort(sort);
+				options[temp] = ""+sort[0]+sort[1];
 				temp++;
 			}
 		}
@@ -14,3 +20,4 @@ public class Calculation {
 		return options;
 	}
 }
+
