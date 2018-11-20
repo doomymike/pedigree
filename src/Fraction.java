d1fb7e92ebe62b762158ffc50e85df7e458cfbee
Tanngent <maxtang77@gmail.com>
2018-11-19 22:04:24 -0500

    Added set, simplified add and multiply

--- src/Fraction.java
+++ src/Fraction.java
@@ -13,24 +13,31 @@ public class Fraction {
 		return ans;
 	}
 	
+	void set(int numerator,int denominaor) {
+		this.numerator = numerator;
+		this.denominaor = denominaor;
+	}
+	
 	double getNumber() {
 		return numerator/denominaor;
 	}
 	
-	void add(Fraction other) {
-		numerator = (numerator * other.denominaor) + (other.numerator * denominaor);
-		denominaor = denominaor * other.denominaor;
-		int GCD = GCD(numerator,denominaor);
-		numerator = numerator/GCD;
-		denominaor = denominaor/GCD;
+	Fraction add(Fraction other) {
+		int top = (numerator * other.denominaor) + (other.numerator * denominaor);
+		int bottom = denominaor * other.denominaor;
+		int GCD = GCD(top,bottom);
+		top = numerator/GCD;
+		bottom = denominaor/GCD;
+		return new Fraction(top,bottom);
 	}
 	
-	void multiply(Fraction other) {
-		numerator = numerator * other.numerator;
-		denominaor = denominaor * other.denominaor;
+	Fraction multiply(Fraction other) {
+		int top = numerator * other.numerator;
+		int bottom = denominaor * other.denominaor;
 		int GCD = GCD(numerator,denominaor);
-		numerator = numerator/GCD;
-		denominaor = denominaor/GCD;
+		top = numerator/GCD;
+		bottom = denominaor/GCD;
+		return new Fraction(top,bottom);
 	}
 	
 	public int GCD(int a, int b) {
