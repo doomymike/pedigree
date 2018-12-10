71621ad639d73912d00be6e8afafb9b7deca4d8a
Nicholas Carr <ncarr@users.noreply.github.com>
2018-12-10 10:56:51 -0500

    Computer machine broke

--- src/Panel.java
+++ src/Panel.java
@@ -33,29 +33,67 @@ public class Panel extends JPanel implements Refreshable {
 			add(lastGeneration);
 		}
 
-		Person person = tree.initialPerson;
-		switch (inheritance) {
-			case AUTOSOMAL_DOMINANT:
-				tree.autosomalDominant(person);
-				break;
-			case AUTOSOMAL_RECESSIVE:
-				tree.autosomalRecessive(person);
-				break;
-			case X_LINKED_DOMINANT:
-				tree.xLinkDominant(person);
-				break;
-			case X_LINKED_RECESSIVE:
-				tree.xLinkRecessive(person);
-				break;
-			case Y_LINKED:
-				tree.yLinked(person);
-				break;
-		}
+		/* calculate();
 
+		Person person = tree.initialPerson;
 		add(new JLabel("Homozygous affected chance: " + person.getHomoAffected().numerator + " / " + person.getHomoAffected().denominator));
 		add(new JLabel("Heterozygous chance: " + person.getHetero().numerator + " / " + person.getHetero().denominator));
-		add(new JLabel("Homozygous unaffected chance: " + person.getHomoUnaffected().numerator + " / " + person.getHomoUnaffected().denominator));
+		add(new JLabel("Homozygous unaffected chance: " + person.getHomoUnaffected().numerator + " / " + person.getHomoUnaffected().denominator)); */
 		revalidate();
 		repaint();
+	}
+
+	private boolean calculatePerson(Person person) {
+		switch (inheritance) {
+			case AUTOSOMAL_DOMINANT:
+				return tree.autosomalDominant(person);
+			case AUTOSOMAL_RECESSIVE:
+				return tree.autosomalRecessive(person);
+			case X_LINKED_DOMINANT:
+				return tree.xLinkDominant(person);
+			case X_LINKED_RECESSIVE:
+				return tree.xLinkRecessive(person);
+			case Y_LINKED:
+				return tree.yLinked(person);
+		}
+		return false;
+	}
+
+	private boolean calculatePersonUp(Person person) {
+		switch (inheritance) {
+			case AUTOSOMAL_DOMINANT:
+				return tree.autosomalDominantUp(person);
+			case AUTOSOMAL_RECESSIVE:
+				return tree.autosomalRecessiveUp(person);
+			case X_LINKED_DOMINANT:
+				return tree.xLinkDominantUp(person);
+			case X_LINKED_RECESSIVE:
+				return tree.xLinkRecessiveUp(person);
+			case Y_LINKED:
+				return tree.yLinkUp(person);
+		}
+		return false;
+	}
+
+	public boolean calculate() {
+		for(int i = 1;i<tree.all.size();i++) {
+			for(int j = 0;j<tree.all.get(i).size();j++) {
+				if(!calculatePersonUp(tree.all.get(i).get(j))){
+					System.out.println("error at (" + i + "," + j + ")");
+					tree.reset();
+					return false;
+				}
+			}
+		}
+		for(int i = tree.all.size()-1;i>=0;i--) {
+			for(int j = 0;j<tree.all.get(i).size();j++) {
+				if(!tree.autosomalRecessive(tree.all.get(i).get(j))) {
+					System.out.println("error at (" + i + "," + j + ")");
+					tree.reset();
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
